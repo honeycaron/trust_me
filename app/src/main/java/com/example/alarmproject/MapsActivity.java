441a298c00fbe262a2d72a910d0aa86f9cb51bc4
jw0712 <jiwonchoi712@gmail.com>
2019-12-03 15:42:41 +0900

    solve map problem

--- app/src/main/java/com/example/alarmproject/MapsActivity.java
+++ app/src/main/java/com/example/alarmproject/MapsActivity.java
@@ -2,11 +2,14 @@ package com.example.alarmproject;
 
 import android.location.Address;
 import android.location.Geocoder;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.FrameLayout;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -31,12 +34,17 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         setContentView(R.layout.activity_maps);
 
         editText = (EditText) findViewById(R.id.etmap);
-        button = (Button) findViewById(R.id.button);
+        button = (Button) findViewById(R.id.btnmap);
 
+        FragmentManager fmanager = getSupportFragmentManager();
+        Fragment fragment = fmanager.findFragmentById(R.id.map);
+        SupportMapFragment supportMapFragment = (SupportMapFragment) fragment;
+
+        supportMapFragment.getMapAsync(this);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+       /* SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+        mapFragment.getMapAsync(this);*/
     }
 
 
